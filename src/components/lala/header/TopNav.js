11f503895178860c03384084c097390eb8857fc6
LalaChristl <lesleyannchristl@gmail.com>
2022-11-18 00:21:53 +0100

    carousel

--- src/components/lala/header/TopNav.js
+++ src/components/lala/header/TopNav.js
@@ -2,19 +2,26 @@ import React from "react";
 import {SlBubbles} from 'react-icons/sl'
 import {BiSearch} from 'react-icons/bi'
 import {GiEarthAmerica} from 'react-icons/gi'
+import {AiOutlineMore} from 'react-icons/ai'
+import {MdOutlineKeyboardArrowDown} from 'react-icons/md'
 
 const TopNav = () => {
   return <div className="bg-[#2B3947] h-[30px] flex justify-end">
     <ul className="flex items-center flex-row gap-3 text-[#C3CBD7] text-[11px] self-center p-3">
-      <li className="border-r-2 border-dotted">Corporate Club</li>
-      <li className="border-r-2 border-dotted">Miles & Smiles</li>
-      <li className="border-r-2 border-dotted flex items-center">
+      <li className="hover:text-[#6FB2A4]">Corporate Club</li>
+      <AiOutlineMore/>
+      <li className="hover:text-[#6FB2A4]">Miles & Smiles</li>
+      <AiOutlineMore/>
+      <li className="flex items-center gap-2">
         <SlBubbles/>
-        <p>Feedback</p></li>
-      <li className="border-r-2 border-dotted flex items-center gap-2">
+        <p className="hover:text-[#6FB2A4]">Feedback</p></li>
+        <AiOutlineMore/>
+      <li className="flex items-center gap-2">
         <BiSearch style={{color:'white'}}/>
-        <p>Search</p></li>
-      <li className="flex items-center gap-2"><GiEarthAmerica/><p>Language</p></li>
+        <p className="hover:text-[#6FB2A4]">Search</p></li>
+        <AiOutlineMore/>
+      <li className="flex items-center gap-2"><GiEarthAmerica/><p className="hover:text-[#6FB2A4]">EN - DE</p>
+      <MdOutlineKeyboardArrowDown/></li>
     </ul>
   </div>;
 };
