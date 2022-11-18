9c4a69553c6b459d0b67baa1a25f5ef889635265
Saad Malik <saad_malik200@hotmail.com>
2022-11-18 23:06:33 +0100

    search container

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../lala/header/Header";
+import SearchContainer from "../saad/searchflights/SearchContainer";
 
 const HomePage = () => {
-  return <div><Header/></div>;
+  return (
+    <div>
+      <Header />
+      <SearchContainer />
+    </div>
+  );
 };
 
 export default HomePage;
