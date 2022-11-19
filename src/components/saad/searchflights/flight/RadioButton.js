4bf9fa38e680b57c83f81422c1d9bb743aaf59ce
Saad Malik <saad_malik200@hotmail.com>
2022-11-20 00:37:56 +0100

    fixing radio

--- src/components/saad/searchflights/flight/RadioButton.js
+++ src/components/saad/searchflights/flight/RadioButton.js
@@ -3,14 +3,24 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
+import { Context } from "../../../context/Context";
 
 export default function RowRadioButtonsGroup() {
+  const { state, dispatch } = React.useContext(Context);
+  console.log(state);
+
+  const [value, setValue] = React.useState("One Way");
+  console.log(value);
+
   return (
     <FormControl>
       <RadioGroup
+        value={value}
+        // defaultValue="One Way"
         row
         aria-labelledby="demo-row-radio-buttons-group-label"
         name="row-radio-buttons-group"
+        onChange={(e) => setValue(e.target.value)}
       >
         <FormControlLabel
           value="Round trip"
@@ -29,7 +39,6 @@ export default function RowRadioButtonsGroup() {
           value="One way"
           control={
             <Radio
-              checked
               sx={{
                 "&.Mui-checked": {
                   color: "#00548A",
